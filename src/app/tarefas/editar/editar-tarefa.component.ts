87c8c52d90eaabf727cb1ebc29a7fb4b13aa8fe2
alesandramempis <alesandramempis@gmail.com>
2021-09-21 13:12:43 -0300

    Implementação do component editar

--- src/app/tarefas/editar/editar-tarefa.component.ts
+++ src/app/tarefas/editar/editar-tarefa.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Tarefa, TarefaService } from '../shared';
 
 @Component({
   selector: 'app-editar-tarefa',
@@ -7,9 +10,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EditarTarefaComponent implements OnInit {
 
-  constructor() { }
+  @ViewChild('formTarefa', { static: true })
+  formTarefa!: NgForm;
 
-  ngOnInit(): void {
+  tarefa!: Tarefa;
+
+  constructor(
+    private tarefaService: TarefaService,
+    private route: ActivatedRoute,
+    private router: Router,
+
+  ) { }
+
+  ngOnInit() {
+    const id = +this.route.snapshot.params['id'];
+    const tarefa = this.tarefaService.buscarPorId(id);
+
+    if(tarefa) {
+      this.tarefa = tarefa;
+    }
+  }
+
+  atualizar(): void {
+    if(this.formTarefa.form.valid) {
+      this.tarefaService.atualizar(this.tarefa);
+      this.router.navigate(['/tarefas'])
+    }
   }
 
 }
+
